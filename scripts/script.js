d8d7d828b51631eba6641a3085fe2d13dd7446e9
Mubarak250 <mubarakbolu150@gmail.com>
2024-09-18 10:58:19 +0100

    improved the html code and made the buttons interactive

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
-const randomNumber = Math.random();
 let computerMove;
 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
+  const randomNumber = Math.random();
   if (randomNumber <= 1/3) {
     computerMove = 'rock';
   } else if (randomNumber <= 2/3) {
@@ -20,31 +22,66 @@ function getComputerChoice() {
 
 function playGame(playerMove) {
   getComputerChoice();
+  const round = 5;
+
 
   if (playerMove === 'rock') {
     if (computerMove === 'rock') {
-      console.log('Tie');
+      document.querySelector('.result').innerHTML = 'It\'s a Tie! ';
     } else if (computerMove === 'paper') {
-      console.log('You Lose');
+      document.querySelector('.result').innerHTML = 'You Lost This Round!, Paper Beats Rock';
+      computerScore++;
     } else if (computerMove === 'scissors') {
-      console.log('You Win');
+      document.querySelector('.result').innerHTML = 'You Win This Round!, Rock Beats Scissors';
+      humanScore++;
     }
   } else if (playerMove === 'paper') {
     if (computerMove === 'rock') {
-      console.log('You Win');
+      document.querySelector('.result').innerHTML = 'You Win This Round!, Paper Beats Rock';
+      humanScore++;
     } else if (computerMove === 'paper') {
-      console.log('Tie');
+      document.querySelector('.result').innerHTML = 'It\'s a Tie! ';
     } else if (computerMove === 'scissors') {
-      console.log('You Lose');
+      document.querySelector('.result').innerHTML = 'You Lost This Round!, Scissors Beats Paper';
+      computerScore++;
     }
   } else if (playerMove === 'scissors') {
     if (computerMove === 'rock') {
-      console.log('You Lose');
+      document.querySelector('.result').innerHTML = 'You Lost This Round!, Rock Beats Scissors';
+      computerScore++;
     } else if (computerMove === 'paper') {
-      console.log('You Win');
+      document.querySelector('.result').innerHTML = 'You Win This Round!, Scissors Beats Paper';
+      humanScore++;
     } else if (computerMove === 'scissors') {
-      console.log('Tie');
+      document.querySelector('.result').innerHTML = 'It\'s a Tie!';
     }
   }
-  console.log(computerMove, playerMove);
+
+  document.querySelector('.scores').innerHTML = `SCORE: You ${humanScore} - ${computerScore} Computer`;
+
+
+  // console.log(computerMove, playerMove);
+  // console.log(humanScore)
+  // console.log(computerScore)
+
+
+  if (humanScore === round && computerScore < round) {
+    document.querySelector('.scores').innerHTML = 'You Win';
+
+    document.querySelector('.result').innerHTML = `You ${humanScore} - ${computerScore} Computer`;
+
+    humanScore = 0;
+    computerScore = 0;
+  } else if (computerScore === round && humanScore < round) {
+    document.querySelector('.scores').innerHTML = 'You Lose';
+
+    document.querySelector('.result').innerHTML = `You ${humanScore} - ${computerScore} Computer`;
+
+    humanScore = 0;
+    computerScore = 0;
+  }
+
+
+  
 }
+
